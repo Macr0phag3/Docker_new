bb5bd0c8b7efc5bbbea52c4f8e7bfa8c6ce2058e
Macr0phag3 <997334636@qq.com>
2018-09-26 17:36:33 +0800

    upload latest code

--- mdtool.py
+++ mdtool.py
@@ -502,7 +502,7 @@ def nk_menu():
 
         for image in IPs:
             print u"[+]镜像名: %s" % pt.put_color(image, "white")
-            for ip in image["ips"]:
+            for ip in IPs[image]["ips"]:
                 print ip
             print "-"*10, pt.put_color(str(len(image["ips"])), "cyan"), "-"*10
 
